71294c877efb3ac907522ab49443657a65d28ef3
Emilia Zysk <emzysk.dev@gmail.com>
2023-09-11 16:49:44 +0900

    add class App

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello from index ts");
+// console.log("hello from index ts");
+
+import express, { Application, Request, Response } from "express";
+
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+  }
+
+  protected routes(): void {
+    this.app.route("/").get((req: Request, res: Response) => {
+      res.send("homepage");
+    });
+  }
+}
